c0b5f1df8f074baf747ad9309fab337fa265f043
Eduardo Ferreira Jaworiwski <89814995+eduardoferreirajaworiwski@users.noreply.github.com>
2023-05-24 20:33:57 -0300

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,5 @@
+package facade_java;
+
+public interface Shape {
+    void draw();
+}
---
+++ Square.java
@@ -0,0 +1,9 @@
+package facade_java;
+
+public class Square implements Shape {
+    @Override
+
+    public void draw() {
+        System.out.println("Square::draw()");
+    }
+}
